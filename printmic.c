fcd27396ef40a99eb9de3b8b23585bf19ccedce7
Zhehao Mao <zhehao.mao@gmail.com>
2012-06-21 20:35:23 -0400

    make printmic use alsahelpers

--- printmic.c
+++ printmic.c
@@ -1,4 +1,4 @@
-#include <alsa/asoundlib.h>
+#include "alsahelpers.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,11 +7,10 @@ void die(char * s, int rc){
 	exit(1);
 }
 
-int main(void){
+int main(int argc, char *argv[]){
 	int rc;
 	snd_pcm_t *handle;
 	snd_pcm_hw_params_t *params;
-	unsigned int val;
 	int dir;
 	snd_pcm_uframes_t frames;
 	short *buffer;
@@ -22,22 +21,9 @@ int main(void){
 	if(rc < 0)
 		die("Could not open pcm device", rc);
 
-	snd_pcm_hw_params_any(handle, params);
+	snd_pcm_hw_params_alloca(&params);
 
-	snd_pcm_hw_params_set_access(handle, params, 
-								 SND_PCM_ACCESS_RW_INTERLEAVED);
-
-	snd_pcm_hw_params_set_format(handle, params, SND_PCM_FORMAT_S16_LE);
-
-	snd_pcm_hw_params_set_channels(handle, params, 1);
-
-	val = 44100;
-	snd_pcm_hw_params_set_rate_near(handle, params, &val, &dir);
-
-	frames = 32;
-	snd_pcm_hw_params_set_period_size_near(handle, params, &frames, &dir);
-
-	rc = snd_pcm_hw_params(handle, params);
+	rc = setup_hardware(handle, params, &dir);
 	
 	if(rc < 0)
 		die("Could not set hardware parameters", rc);
